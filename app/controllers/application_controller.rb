9e64976e826d179bf3341610aeb5a53c41f45bcb
Calvin Szeto <szeto.calvin@gmail.com>
2015-04-28 08:41:57 -0400

    Add authorize code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,25 +12,29 @@ class ApplicationController < ActionController::Base
 
 	def request_token
 		callback_url = "#{root_url}callback"
-		session[:request_token] = @user.evernote_client.request_token(
+		session[:authorize_url] = @user.evernote_client.request_token(
 			oauth_callback: callback_url
-	  )
+	  ).authorize_url
 		redirect_to '/authorize'
 	end
 
 	def authorize
-		binding.pry
-		if session[:request_token]
-			redirect_to session[:request_token].authorize_url
+		if session[:authorize_url]
+			redirect_to session[:authorize_url]
 		else
-			render nothing: true
+			redirect_to '/request_token'
 		end
 	end
 
-	def callback
+	def callback 
+		unless params[:oauth_verifier] || session[:authorize_url]
+			redirect_to '/request_token'
+		end
+		session[:access_token] = session[:request_token].get_access_token(:oauth_verifier => session[:oauth_verifier])
+		redirect '/list'
 	end
 
-	private
+private
 
 	def get_user
 		@user = User.first || User.create
